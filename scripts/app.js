9aeccdac77d2f53bb6da0e8ec763489a0c66b0f9
KostadinovK <kostadinovk2001@gmail.com>
2019-11-25 17:08:27 +0200

    Added the register form

--- scripts/app.js
+++ scripts/app.js
@@ -20,6 +20,15 @@ const app = Sammy('#main', function(){
         });
     });
 
+    this.get('#/register', (context) => {
+        context.loadPartials({
+            header: './views/common/header.hbs',
+            footer: './views/common/footer.hbs'
+        }).then(function(){
+            this.partial('./views/user/register.hbs');
+        });
+    })
+
 });
 
 (() => {
