49855639ad1973d87e55949d0373ab6b4bce270b
KostadinovK <kostadinovk@gmail.com>
2020-01-27 21:22:43 +0200

    If user is already logged in on app opening the redirecting is to the dashboard

--- scripts/controllers/homeController.js
+++ scripts/controllers/homeController.js
@@ -1,6 +1,13 @@
 const homeController = function() {
     
     const getHome = function(context){
+        let loggedIn = globalConstants.IsLoggedIn();
+
+        if(loggedIn){
+            context.redirect('#/dashboard');
+            return;
+        }
+
         context.loadPartials({
             header: './views/common/header.hbs',
             footer: './views/common/footer.hbs'
